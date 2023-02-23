8fd52f60c8b6124f812edf8749c8f20e0f07895f
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-23 21:44:12 +0530

    Added Contact Details To Table

--- AddressBook_SQLQuery1.sql
+++ AddressBook_SQLQuery1.sql
@@ -2,18 +2,26 @@
 create Database Address_Book_Service_DB;
 
 --UseCase2- Create A table--
-Create Table AddressBookTable
+Create Table AddressBookTable1
 (
-FirstName VARCHAR(15) NOT NULL,
+FirstName VARCHAR(20) NOT NULL,
 LastName VARCHAR(20) NOT NULL,
 Address VARCHAR(50) NOT NULL,
-City VARCHAR(15) NOT NULL,
-State VARCHAR(15) NOT NULL,
-Zip Int NOT NULL,
-PhoneNumber Int NOT NULL,
+City VARCHAR(20) NOT NULL,
+State VARCHAR(20) NOT NULL,
+Zip BIGINT,
+PhoneNumber BIGINT,
 Email VARCHAR(30) NOT NULL
-)
-Select * From AddressBookTable;
+);
+Select * From AddressBookTable1;
+
+--UseCase3-insert new Contacts to AddressBookTable
+INSERT INTO AddressBookTable1(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+VALUES('Marina','Singh','Colony','Pune','Maharashtra',123456,9876543210,'marina@gmail.com'),
+('Shreeram','Patil','Street','Noida','Delhi',410506,9876512345,'shreeram@gmail.com'),
+('Harisri','Kiran','Nagar','Bangalore','Karnataka',321456,7418529613,'kiran@gmail.com'),
+('Vidan','Guru','Town','Hyderabda','Telangana',765432,9123457689,'vidan@gmail.com');
+Select * From AddressBookTable1;
 
 
 
