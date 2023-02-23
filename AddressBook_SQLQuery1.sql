bcb7835c3e222e98300e4c136ebec65f370caebe
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-23 21:11:43 +0530

    Added Create a AddressBookTable

--- AddressBook_SQLQuery1.sql
+++ AddressBook_SQLQuery1.sql
@@ -1,7 +1,19 @@
 --UseCase1-Create a data base--
 create Database Address_Book_Service_DB;
 
-
+--UseCase2- Create A table--
+Create Table AddressBookTable
+(
+FirstName VARCHAR(15) NOT NULL,
+LastName VARCHAR(20) NOT NULL,
+Address VARCHAR(50) NOT NULL,
+City VARCHAR(15) NOT NULL,
+State VARCHAR(15) NOT NULL,
+Zip Int NOT NULL,
+PhoneNumber Int NOT NULL,
+Email VARCHAR(30) NOT NULL
+)
+Select * From AddressBookTable;
 
 
 
