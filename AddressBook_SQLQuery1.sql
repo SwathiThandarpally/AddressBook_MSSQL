40e03ffe7e8ce3e7fe2ede77501a0d1847d50fab
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-23 21:48:59 +0530

    Added Edit Existing Contact Details

--- AddressBook_SQLQuery1.sql
+++ AddressBook_SQLQuery1.sql
@@ -15,13 +15,16 @@ Email VARCHAR(30) NOT NULL
 );
 Select * From AddressBookTable1;
 
---UseCase3-insert new Contacts to AddressBookTable
+--UseCase3- Insert new Contacts to AddressBookTable
 INSERT INTO AddressBookTable1(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
 VALUES('Marina','Singh','Colony','Pune','Maharashtra',123456,9876543210,'marina@gmail.com'),
 ('Shreeram','Patil','Street','Noida','Delhi',410506,9876512345,'shreeram@gmail.com'),
 ('Harisri','Kiran','Nagar','Bangalore','Karnataka',321456,7418529613,'kiran@gmail.com'),
 ('Vidan','Guru','Town','Hyderabda','Telangana',765432,9123457689,'vidan@gmail.com');
 Select * From AddressBookTable1;
+
+--UseCase4- Edit Contact
+UPDATE AddressBookTable1 SET Zip = 411506 WHERE LastName = 'Guru';
 
 
 
